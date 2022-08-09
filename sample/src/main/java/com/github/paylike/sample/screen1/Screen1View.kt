071472c2e51a31ee2069396a5217d206ea4c3dca
Balazs Radvanyi <balazs@riltech.co>
2022-08-09 14:38:22 +0200

    feat(ui): started implementing the first screen

--- sample/src/main/java/com/github/paylike/sample/screen1/Screen1View.kt
+++ sample/src/main/java/com/github/paylike/sample/screen1/Screen1View.kt
@@ -1,18 +1,39 @@
 package com.github.paylike.sample.screen1
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Screen1() {
-   // Button(
-   //     onClick = { /* ... */ },
-   // ) {
-   //     Text("Pay")
-   // }
-    Text("Baszódjál meg te.")
+    Column(
+        modifier = Modifier.fillMaxSize(), // 1
+        horizontalAlignment = Alignment.CenterHorizontally, // 2
+        verticalArrangement = Arrangement.Center // 3
+    ) {
+        Text(
+            "TransactionID:",
+            fontSize = 30.sp,
+        )
+        Text(
+            "Error:",
+            fontSize = 30.sp,
+        )
+        Button(
+            onClick = { /* ... */ },
+        ) {
+            Text("Pay")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
